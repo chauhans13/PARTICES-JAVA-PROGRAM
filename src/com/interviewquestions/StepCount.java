f51ccbae9f924467527777643cbff78970b75831
Mehtaab <mehtaab@ericsson.com>
2019-10-07 09:06:56 +0530

    adds EvalPostfix MaxSumPath StepCount

---
+++ src/com/interviewquestions/StepCount.java
@@ -0,0 +1,15 @@
+package com.interviewquestions;
+
+public class StepCount {
+	public static void main(String args[]) {
+		/*
+		 * A child is running up a staircase with n steps and can hop either 1
+		 * step or 2 steps at a time. Implement a method to count how many
+		 * possible ways the child can run up the stairs
+		 * 
+		 * Input : 4 Output : 7
+		 * 
+		 * Input : 3 Output : 4
+		 */
+	}
+}
